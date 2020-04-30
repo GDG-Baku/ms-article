37e19c7e21068c0d0c99f5b24d352db093aae57a
alihsoff <alihsoff@gmail.com>
2020-04-30 19:17:41 +0400

    BE-62: delete comments before article

--- src/main/java/az/gdg/msarticle/service/impl/ArticleServiceImpl.java
+++ src/main/java/az/gdg/msarticle/service/impl/ArticleServiceImpl.java
@@ -3,7 +3,10 @@ package az.gdg.msarticle.service.impl;
 import az.gdg.msarticle.exception.NoAccessException;
 import az.gdg.msarticle.exception.NoSuchArticleException;
 import az.gdg.msarticle.exception.NotValidTokenException;
+import az.gdg.msarticle.model.entity.ArticleEntity;
+import az.gdg.msarticle.model.entity.CommentEntity;
 import az.gdg.msarticle.repository.ArticleRepository;
+import az.gdg.msarticle.repository.CommentRepository;
 import az.gdg.msarticle.service.ArticleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,15 +14,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class ArticleServiceImpl implements ArticleService{
     private static final Logger logger = LoggerFactory.getLogger(ArticleServiceImpl.class);
     private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
 
 
-    public ArticleServiceImpl(ArticleRepository articleRepository) {
+    public ArticleServiceImpl(ArticleRepository articleRepository, CommentRepository commentRepository) {
         this.articleRepository = articleRepository;
+        this.commentRepository = commentRepository;
     }
 
 
@@ -27,17 +34,29 @@ public class ArticleServiceImpl implements ArticleService{
     public void deleteArticleById(String articleID) {
         logger.info("ActionLog.deleteArticleById.start");
         int userId = Integer.parseInt((String)getAuthenticatedObject().getPrincipal());
-        int articleUserId = articleRepository.findById(articleID)
-                .orElseThrow(()-> new NoSuchArticleException("Article doesn't exist")).getUserId();
+        ArticleEntity articleEntity = articleRepository.findById(articleID)
+                .orElseThrow(() -> new NoSuchArticleException("Article doesn't exist"));
+        int articleUserId = articleEntity.getUserId();
         if(userId == articleUserId) {
+            // deleting comments and replies before delete article - start
+            List<CommentEntity> commentEntityList = articleEntity.getComments();
+            for (CommentEntity commentEntity : commentEntityList) {
+                if (commentEntity.getReplies() != null) {
+                    commentRepository.deleteAll(commentEntity.getReplies());
+                }
+            }
+            commentRepository.deleteAll(commentEntityList);
+            // end
             articleRepository.deleteById(articleID);
         } else {
+            logger.info("Thrown.NoAccessException");
             throw new NoAccessException("You don't have permission to delete the article");
         }
     }
 
     private Authentication getAuthenticatedObject() {
         if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            logger.info("Thrown.NotValidTokenException");
             throw new NotValidTokenException("Token is not valid or it is expired");
         }
         return SecurityContextHolder.getContext().getAuthentication();
