8efe36098d714c9b2532b11a6cac2ede7470bb1c
muradisgandar <isgandarli_murad@mail.ru>
2020-05-01 11:42:55 +0400

    BE-57: seperate config classes for feign and mongodb

--- src/main/java/az/gdg/msarticle/MsArticleApplication.java
+++ src/main/java/az/gdg/msarticle/MsArticleApplication.java
@@ -2,11 +2,7 @@ package az.gdg.msarticle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@EnableMongoAuditing
-@EnableFeignClients
 @SpringBootApplication
 public class MsArticleApplication {
 
