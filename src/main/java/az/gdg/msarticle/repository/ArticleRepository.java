0a6598f120b8dcbb5991f80a35e499e212b2f430
alihsoff <alihsoff@gmail.com>
2020-05-28 02:20:27 +0400

    BE-62: fix checkstyle issues

--- src/main/java/az/gdg/msarticle/repository/ArticleRepository.java
+++ src/main/java/az/gdg/msarticle/repository/ArticleRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ArticleRepository extends MongoRepository<ArticleEntity,String> {
+public interface ArticleRepository extends MongoRepository<ArticleEntity, String> {
 
     List<ArticleEntity> getArticleEntitiesByUserId(Integer userId);
 
--- src/main/java/az/gdg/msarticle/service/ArticleService.java
+++ src/main/java/az/gdg/msarticle/service/ArticleService.java
@@ -1,7 +1,5 @@
 package az.gdg.msarticle.service;
 
-import az.gdg.msarticle.model.dto.UserArticleDTO;
-
 public interface ArticleService {
 
     void deleteArticleById(String articleId);
