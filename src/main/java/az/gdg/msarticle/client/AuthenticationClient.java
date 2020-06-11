c8d30e28440088cb63c81db9b11ee44cb5f2f6d8
asifhajiyev <asifhaciyev1498@gmail.com>
2020-06-11 19:27:09 +0400

    BE-70: adding client url to yml file

--- src/main/java/az/gdg/msarticle/client/AuthenticationClient.java
+++ src/main/java/az/gdg/msarticle/client/AuthenticationClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import static az.gdg.msarticle.model.client.auth.HttpHeader.X_AUTH_TOKEN;
 
-@FeignClient(value = "ms-auth-client", url = "https://gdg-ms-auth.herokuapp.com/")
+@FeignClient(value = "ms-auth-client", url = "${client.service.url.ms-auth}")
 public interface AuthenticationClient {
     @GetMapping("/user/info")
     UserInfo getUserInfo(@RequestHeader(X_AUTH_TOKEN) String token);
